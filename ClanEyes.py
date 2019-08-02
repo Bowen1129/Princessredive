e528da511501651e3f18593f04f7fbfcb26064b7
Bowen1129 <53566134+Bowen1129@users.noreply.github.com>
2019-08-02 16:59:07 +0800

    Add files via upload

--- ClanEyes.py
+++ ClanEyes.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
 import re
+import numpy as np
 import pandas as pd
-
+from itertools import islice
 #读取文件
-with open('D:\\shit\\原始数据.txt',encoding= 'utf-8') as origin_file:
+a=[]
+b=[]
+with open('原始数据.txt',encoding= 'utf-8',) as origin_file:
     for line in origin_file.readlines():
-        line=line.strip()
-        # print(line)
+        if re.match('(.*が$)', line) or re.match('.*ダメージ*.', line) or re.match('.*に$', line):
+            a=line
+            f1=open("washup.txt","a+",encoding='utf-8')
+            f1.write(line)
+            f1.close()
+    # print(a)
+    # while True:
+    #     line2=list(islice(a,3))
+    #     if not a:
+    #         break
+    #     b.append(line2)
+    #     print(b)
+# data_df=pd.DataFrame(a)
+# data_df=data_df.replace('\n','')
+# print(data_df)
+# data_df.to_csv('washup.csv',encoding='utf-8-sig')
 #匹配信息
-        if re.match('(.*が$)', line) or re.match('.*ダメージ$', line ) or re.match('.*に$', line ):
-            print(line.strip())
+# data_df = pd.DataFrame(data=info, columns=['Name', 'Damage', 'Boss'])
+# print(data_df)
+    # data_dfname.to_csv('damage.csv')
+# print(re.match('(.*が$)', line))
+# print(data_df)
